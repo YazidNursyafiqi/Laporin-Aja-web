2b503f510fae390c3d257fb17d1b3e04b84fbb1c
Yazid Nursyafiqi <yazidnursyafiqi007@gmail.com>
2026-01-06 20:19:16 +0800

    perbaiki dikit

--- LaporinAja/src/pages/report/reports.jsx
+++ LaporinAja/src/pages/report/reports.jsx
@@ -1,7 +1,10 @@
 import { use } from "react";
 import { useState , useEffect } from "react";
+import styles from './reports.module.css'
+
 
 function Reports(){
+    //data form yang value nya berupa string di simpan sementara di sini sebelum di submit
     const [ form , setForm ] = useState({
         kirim_sebagai:"",
         jenis_pengaduan:"",
@@ -11,13 +14,16 @@ function Reports(){
         yang_terdampak:""
     })
 
+    //fungsi untuk handle preview gambar
     const [ preview , setPreview ] = useState(null);
     const handleImage = (e) => {
         const path = e.target.files[0]
         const imageurl = URL.createObjectURL(path)
         setPreview(imageurl)
+        //console.log(path.type,path.size)
     }
 
+    //fungsi untuk update objek yang berisi data form secara realtime saat user mmengubah/modif isi form
     const handleChange = (e)=>{
         const name = e.target.name
         const value = e.target.value
@@ -28,6 +34,7 @@ function Reports(){
         
     }
 
+    
     useEffect(()=>{
         return()=>{
             URL.revokeObjectURL(preview)
@@ -44,60 +51,79 @@ function Reports(){
         for (const key in form){
             console.log(key," ",form[key])
         }
+        formData.append()
     }
     
 
     return(
-        <form onSubmit={submitData}>
-            <label>
-                Kirim sebagai:
-                <select value={form.kirim_sebagai} name="kirim_sebagai" onChange={handleChange}>
-                    <option value="Anonim">Anonim</option>
-                    <option value="User">User</option>
-                </select>
-            </label>
-            <br/>
-            <label>
-                Masukkan jenis Pengaduan:
-                <select value={form.jenis_pengaduan} name="jenis_pengaduan" onChange={handleChange}>
-                    {opsi_pengaduan.map((value)=>(
-                        <option value={value} key={value}>{value}</option>
-                    ))}
-                </select>
-            </label>
-            <br/>
-            <label>
-                Berikan penjelasan selengkap mungkin
-                <br/>
-                 <textarea type="textarea" rows="5" cols="50" value={form.penjelasan} name="penjelasan" onChange={handleChange}/>
-            </label>
-            <br/>
-            <label>
-                Kondisi masalah saat ini:
-                <select value={form.kondisi_saat_ini} name="kondisi_saat_ini" onChange={handleChange}>
-                    <option value="Belum-terselesaikan">Belum Terselesaikan</option>
-                    <option value="Tidak-Diselesaikan">Tidak di selesaikan sama sekali</option>
-                </select>
-            </label>
-            <br/>
+        <form onSubmit={submitData} id={styles.container}>
+            <div id={styles.dropdownMenu}>
+                <div className={styles.dropdownSide}>
+                    <div className={styles.formInput}>
+                        <label>
+                            Kirim sebagai:
+                            <select value={form.kirim_sebagai} name="kirim_sebagai" onChange={handleChange}>
+                                <option value="Anonim">Anonim</option>
+                                <option value="User">User</option>
+                            </select>
+                        </label>    
+                    </div>
+                    
+                    <div className={styles.formInput}>
+                        <label>
+                            Masukkan jenis Pengaduan:
+                            <select value={form.jenis_pengaduan} name="jenis_pengaduan" onChange={handleChange}>
+                                {opsi_pengaduan.map((value)=>(
+                                    <option value={value} key={value}>{value}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </div>
+                    
+                    <div className={styles.formInput}>
+                        <label>
+                            Berikan penjelasan selengkap mungkin
+                            <br/>
+                            <textarea type="textarea" rows="5" cols="50" value={form.penjelasan} name="penjelasan" onChange={handleChange}/>
+                        </label>
+                    </div>
+                    
+                </div>
+                <div className={styles.dropdownSide}>
+                    <div className={styles.formInput}>
+                        <label>
+                            Kondisi masalah saat ini:
+                            <select value={form.kondisi_saat_ini} name="kondisi_saat_ini" onChange={handleChange}>
+                                <option value="Belum-terselesaikan">Belum Terselesaikan</option>
+                                <option value="Tidak-Diselesaikan">Tidak di selesaikan sama sekali</option>
+                            </select>
+                        </label>
+                    </div>
+                    
+                    <div className={styles.formInput}>
+                        <label>
+                            Siapa saja yang terdampak masalah saat ini:
+                            <select value={form.yang_terdampak} name="yang_terdampak" onChange={handleChange}>
+                                <option value="saya-sendiri">Saya sendiri</option>
+                                <option value="Semua-Masyarakat">Semua Masyarakat</option>
+                            </select>
+                        </label>
+                    </div>
+                    
+                </div>
+            </div>
             <label>
                 Masukkan bukti jika ada
                 <br />
                 <input type="file" onChange={handleImage}/>
-                <img src={preview}/>
+                <div id={styles.imagePreview}>
+                    <img src={preview}/>
+                </div>
             </label>
             <br />
             <label>
                 Siapa saja yang terkait dengan masalah ini
                 <input type="text" />
-            </label>
-            <br />
-            <label>
-                Siapa saja yang terdampak masalah saat ini:
-                <select value={form.yang_terdampak} name="yang_terdampak" onChange={handleChange}>
-                    <option value="saya-sendiri">Saya sendiri</option>
-                    <option value="Semua-Masyarakat">Semua Masyarakat</option>
-                </select>
             </label>
             <br />
             <input type="submit" value="kirim"/>
