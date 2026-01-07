97c69ec39e593aceee8f58f1ec5709dd4747d2de
Yazid Nursyafiqi <yazidnursyafiqi007@gmail.com>
2026-01-07 21:49:17 +0800

    mengupdate backend agar request dari frontend diterima oleh backend dan di simpan di direktori backend

--- LaporinAja/src/hooks/sendReport.js
+++ LaporinAja/src/hooks/sendReport.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL
 
 function sendReport(formData){
-    axios.post(`${API_BASE_URL}/Report`,formData)
+    axios.post(`${API_BASE_URL}/upload`,formData)
 }
 
 export default sendReport
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,15 +1,33 @@
 import express from "express"
 import multer from "multer"
+import { diskStorage } from "multer"
+import cors from "cors"
 
-const upload = multer({dest:"../uploads/"})
+const storage = multer.diskStorage({
+    destination : (req,file,cb)=>{
+        cb(null,"uploads/")
+    },
+    filename : (req,file,cb)=>{
+        const cap = Date.now()
+        const name = cap + file.originalname
+        cb(null,name)
+    }
+})
+
+const upload = multer({storage:storage})
 const app = express()
 
+app.use(cors())
+
 app.post("/upload",upload.single("image"),(req,res)=>{
+    console.clear()
     console.log("file:")
     console.log(req.file)
     console.log("gambar:")
     console.log(req.body)
-    res.end("selesai")
+    res.status(200).json({
+        message:"selesai wak!!"
+    })
 })
 
 app.listen(3000,()=>{
