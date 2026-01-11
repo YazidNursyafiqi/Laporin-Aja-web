1058eeedf21a96fe9d3ca2a1d6b72d9e0db33624
Yazid Nursyafiqi <yazidnursyafiqi007@gmail.com>
2026-01-11 19:48:11 +0800

    membuat element post untuk menampilkan masalah

--- LaporinAja/src/component/posts/Post.jsx
+++ LaporinAja/src/component/posts/Post.jsx
@@ -1,8 +1,35 @@
-function Pos(){
+import styles from "./post.module.css"
+
+function Post({from,likes,comments,location,type,perpetrator,victim,explain,image}){
     return(
         <>
+            <div id={styles.container}>
+                <div id={styles.account}>
+                    <img src="/icons/user.png"/>
+                    <p>{from}</p>
+                </div>
+                <div id={styles.explain}>
+                    {explain}
+                </div>
+                <div id={styles.image}>
+                    <img src={image}></img>
+                </div>
+                <div id={styles.perpetrator}>
+                    <p>Siapa saja yang terkait:</p>
+                    <div id={styles.itemContainer}>
+                        {Object.keys(perpetrator).map(key=>(
+                            <>
+                                <div id={styles.item}>
+                                    <p id={styles.itemName}>{key}</p>
+                                    <p id={styles.itemRole}>{perpetrator[key]}</p>
+                                </div>
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export default Pos
+export default Post
--- LaporinAja/src/pages/view-problems/navbar/teratas/teratas.jsx
+++ LaporinAja/src/pages/view-problems/navbar/teratas/teratas.jsx
@@ -1,9 +1,21 @@
+import Post from "../../../../component/posts/Post"
 
 export default function Teratas(){
+    const msg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla arcu eros, congue sit amet ante non, ultrices laoreet nisi. Morbi in sem consequat, eleifend est quis, aliquet erat. Nullam congue "
+    const image = "https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?q=80&w=1184&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const perp = {
+        yazid:"abc",
+        arun:"def",
+        Andi: "Mahasiswa",
+        Budi: "Ketua Kelas",
+        Citra: "Sekretaris",
+        Dewi: "Bendahara",
+        Eko: "Anggota"
+    }
+
     return(
         <>
-            ini teratas
-
+            <Post from="Yazid Nursyafiqi" explain={msg} image={image} perpetrator={perp}/>
         </>
     )
 }
