c5e3dcd21a8191fee7367dc48590c0c4c5d75875
aaronnofrail <arundaffa.nahara@gmail.com>
2026-01-11 16:56:08 +0800

    Add Header and add some features

--- LaporinAja/src/pages/view-problems/navbar/wilayah/wilayah.jsx
+++ LaporinAja/src/pages/view-problems/navbar/wilayah/wilayah.jsx
@@ -1,9 +1,42 @@
 import Maps from "../../../../component/indonesia-map/maps"
+import style from "./wilayah.module.css";
 
 function Wilayah(){
+    // data dummy (nanti bs diganti API)
+    const topRegions = [
+        { name: "Sulawesi Selatan", count: 80 },
+        { name: "Jawa Barat", count: 30 },
+        { name: "Jawa Tengah", count: 30 },
+        { name: "Jawa Timur", count: 30 },
+    ];
+
     return(
         <>  
-            <Maps/>
+            <div className={style.container}>
+                <h1 className={style.title}>Lihat kumpulan pengajuan</h1>
+
+                <div className={style.mapWrapper}>
+                    <Maps />
+                </div>
+
+                <div className={style.topRegionSection}>
+                    <h3 className={style.sectionTitle}>Wilayah Teratas</h3>
+                    
+                    <div className={style.listContainer}>
+                    {topRegions.map((item, index) => (
+                        <div key={index} className={style.listItem}>
+                        <span>{item.name}</span>
+                        <span className={style.count}>{item.count}</span>
+                        </div>
+                    ))}
+                    </div>
+
+                    <div className={style.seeMore}>
+                    <a href="#">See more</a>
+                    </div>
+                </div>
+
+                </div>
         </>
     )
 }
--- LaporinAja/src/pages/view-problems/view-problems.jsx
+++ LaporinAja/src/pages/view-problems/view-problems.jsx
@@ -1,5 +1,6 @@
 import { Link, Route , Routes } from "react-router-dom";
-//import navbar
+//import navbar dan header
+import Header from "../../container/header/header";
 import Navbar from "../../container/navbar/main_navbar";
 import styles from "./view-problems.module.css";
 
@@ -9,14 +10,16 @@ import Wilayah from "./navbar/wilayah/wilayah";
 function ViewProblems_page(){
     return(
         <>
-         <div className={styles.layout}>
-            <Navbar/>
-            <div id="Content">
-                <Routes>
-                    <Route path="/Wilayah" element={<Wilayah/>}/>
-                </Routes>
+         <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+            <Header/>
+            <div className={styles.layout}>
+                <Navbar/>
+                <div id="Content">
+                    <Routes>
+                        <Route path="/Wilayah" element={<Wilayah/>}/>
+                    </Routes>
+                </div>
             </div>
-              
          </div>
         </>
     )
