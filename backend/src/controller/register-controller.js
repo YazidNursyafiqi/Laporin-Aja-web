60b12b4b3fc33bb9a7756f15110f39eac67ffcba
Yazid Nursyafiqi <yazidnursyafiqi007@gmail.com>
2026-01-18 16:02:31 +0800

    menambahkan pengecekan akun sebelumnya dengan username yang sama

--- backend/src/controller/register-controller.js
+++ backend/src/controller/register-controller.js
@@ -1,8 +1,10 @@
 import registerService from "../services/register-service.js"
 
 export const registerController = async(req,res)=>{
-    console.log(req.body,"    .loading")
-    await registerService({...req.body , likes:[] , posts:[]})
-    console.log('the end')
-    res.end("berhasil")
+    try{
+        const result = await registerService({...req.body , likes:[] , posts:[]})
+        res.json(result)
+    }catch{
+        res.json({status:'server-error'})
+    }
 }
--- backend/src/services/register-service.js
+++ backend/src/services/register-service.js
@@ -1,8 +1,15 @@
 import db from "../application/firestore.js"
 
 const registerService = async(input)=>{
-    await db.collection("accounts").add(input)
-    console.log('registrasi berhasil')
+    //mengecek apakah ada akun dengan nama yang sama
+    const snapshot = await db.collection('accounts').where('username','==',input.username).limit(1).get()
+    if(snapshot.empty){
+        await db.collection("accounts").add(input)
+        return {status:'succeed'}
+    }
+    return {status:'username is already taken'}
+
+
 }
 
 export default registerService
