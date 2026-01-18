cb57c6d2daf2523b44fd4ef1cf99d28768a850ec
aaronnofrail <arundaffa.nahara@gmail.com>
2026-01-18 18:30:42 +0800

    ubah deskripsi about page

--- LaporinAja/src/pages/about_page/about_page.jsx
+++ LaporinAja/src/pages/about_page/about_page.jsx
@@ -15,30 +15,30 @@ function About_page(){
                     <h1 className={styles.title}>
                         Suarakan Masalah, Temukan <span className={styles.highlight}> Solusi.</span>
                     </h1>
-                    {/* <p className={styles.subtitle}>
+                    <p className={styles.subtitle}>
                         LaporinAja hadir untuk menjembatani aspirasi masyarakat dengan solusi modern. 
                         Kami percaya bahwa setiap kritik adalah langkah awal menuju perubahan yang lebih baik.
-                    </p> */}
+                    </p>
                     </div>
 
                     <div className={styles.storySection}>
                     <div className={styles.storyContent}>
                         <h2>Mengapa LaporinAja Dibuat?</h2>
 
-                        <p>
+                        {/* <p>
                             Tugas GDGoC
-                        </p>
-                        {/* <p>
-                        Kami menyadari bahwa banyak masalah infrastruktur dan layanan publik yang luput dari perhatian karena sulitnya akses pelaporan di negara ini.  
                         </p> */}
+                        <p>
+                        Kami menyadari bahwa banyak masalah infrastruktur dan layanan publik yang luput dari perhatian karena sulitnya akses pelaporan di negara ini.  
+                        </p>
 
-                        {/* <p>
+                        <p>
                         <strong>LaporinAja</strong> lahir dari keinginan untuk memotong jarak tersebut.
                         </p>
 
                         <p>
                         Dengan teknologi yang mudah diakses, kami ingin memberdayakan setiap individu untuk menjadi agen perubahan di lingkungannya sendiri.
-                        </p> */}
+                        </p>
                     </div>
                     </div>
                 </div>
